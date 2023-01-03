3bb46e4c8fa209c28e8f77da1b83ed5dbaf187b8
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-01-03 16:58:15 -0300

    test: use string instead randomUUID() function in notification test

--- src/application/entities/notification/notification.spec.ts
+++ src/application/entities/notification/notification.spec.ts
@@ -1,16 +1,16 @@
-import { randomUUID } from 'crypto';
 import { Content } from './content';
 import { Notification } from './notification';
 
 describe('Notification', () => {
   it('should be able to create a notification', async () => {
-    const content = new Content('Você recebeu uma solicitação de amizade!');
-    const recipientUUID = randomUUID();
+    const contentInstance = new Content(
+      'Você recebeu uma solicitação de amizade!',
+    );
 
     const notification = new Notification({
       category: 'social',
-      content: content,
-      recipientId: recipientUUID,
+      content: contentInstance,
+      recipientId: 'example-recipient-id',
     });
 
     expect(notification).toBeTruthy();
