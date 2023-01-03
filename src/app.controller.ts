14c3b713faada60ac45b45eb1818a9d38224ee16
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-01-03 15:01:08 -0300

    feat: add CreateNotificationBody dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './dtos/create-notification-body';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -13,15 +14,15 @@ export class AppController {
   }
 
   @Post()
-  async create(@Body() body: any) {
+  async create(@Body() body: CreateNotificationBody) {
     const UUID = randomUUID();
-    const recipientUUID = randomUUID();
+
     await this.prisma.notification.create({
       data: {
         id: UUID,
         category: body.category,
         content: body.content,
-        recipientId: recipientUUID,
+        recipientId: body.recipientId,
       },
     });
   }
